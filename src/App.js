c6079216353b4cf546850b9076165a790744813f
Mohammedrisvanvm <risvanrishu0000@gmail.com>
2023-05-26 14:08:36 +0530

    displaying user name if user is logged in

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
-import React, { useContext, useEffect} from "react";
+import React, { useContext, useEffect } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 import "./App.css";
 import Home from "./Pages/Home";
 import SignupPage from "./Pages/Signup";
 import Login from "./Pages/Login";
-import { AuthContext } from "./store/Context";
+import { AuthContext, FirebaseContext } from "./store/Context";
 
 function App() {
-  const {user}=useContext(AuthContext)
-  useEffect(()=>{
-console.log(user);
-  })
+  const { setUser } = useContext(AuthContext);
+  const { firebase } = useContext(FirebaseContext);
+  useEffect(() => {
+    firebase.auth().onAuthSetChanged((user) => {
+      setUser(user);
+    });
+  });
   return (
     <div>
       <Router>
--- src/store/Context.js
+++ src/store/Context.js
@@ -1,15 +1,13 @@
-import {createContext, useState} from 'react'
-export const FirebaseContext=createContext(null)
-export const AuthContext=createContext(null)
+import { createContext, useState } from "react";
+export const FirebaseContext = createContext(null);
+export const AuthContext = createContext(null);
 
+export default function Context({ children }) {
+  const [user, setUser] = useState(null);
 
-export default function Context({children}){
-    const [user,setUser]=useState('hello')
-
-
-    return (
-<AuthContext.Provider value={{user}}>
-   {children}
-</AuthContext.Provider>
-    )
+  return (
+    <AuthContext.Provider value={{ user, setUser }}>
+      {children}
+    </AuthContext.Provider>
+  );
 }
